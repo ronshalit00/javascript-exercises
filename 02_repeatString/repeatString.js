ebac96102f2cb5b8957cfce6c3d8cee7d5124ee2
ronshalit00 <ronshalit00@gmail.com>
2023-04-15 11:09:43 +0300

    Solves second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let output = "";
+	if (num > -1) {
+		for (let i = 0; i < num; i++) {
+			output += string;
+		}
+	} else {
+		return "ERROR";
+	}
+	return output;
 };
 
 // Do not edit below this line
