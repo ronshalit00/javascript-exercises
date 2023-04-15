c7fa32e07e92e42b4e183ccbfe50ccedc5b55c90
ronshalit00 <ronshalit00@gmail.com>
2023-04-15 15:54:13 +0300

    Solves 7th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (inFar) {
+	const cel = ((inFar - 32) * 5) / 9;
+	let outCel;
+	if (cel > 0 || cel < 0) {
+		outCel = cel.toFixed(1);
+	} else {
+		outCel = cel;
+	}
+	return +outCel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (inCel) {
+	const far = (inCel * 9) / 5 + 32;
+	let outFar;
+	if (far > 0 || far < 0) {
+		outFar = far.toFixed(1);
+	} else {
+		outFar = far;
+	}
+	return +outFar;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
